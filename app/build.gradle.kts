71b0a4e46daf6f738f82008615fecaa6b689f44c
filimonov_ea <filimonov1501@gmail.com>
2023-11-23 00:45:13 +0400

    fix release app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,12 +28,7 @@ android {
     }
 
     buildTypes {
-        debug {
-            isMinifyEnabled = false
-
-        }
         release {
-            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
